10a960ecbbbac108d9150429be9483f55f3f8f1e
James R Holliday <jrholliday@gmail.com>
2013-08-29 12:21:19 -0700

    Rewrote bylaws after talking to California Tax Franchise.
    1) Removed member classifications.  We now have no members.
    2) Changed voting from "Members in Good Standing" to "anyone who
       has attended 2 of most recent 3 public meetings".
    3) Cleaned up language.  Mostly replaced "member" with "person".
    4) Rearranged a few articles.  Just to mess with diffs ;^)

--- html/js/bylaws.js
+++ html/js/bylaws.js
@@ -1,35 +1,35 @@
 $(document).ready(function() {
-    var counter_h1 = 0;
-    var counter_h2 = 0;
     var counter_h3 = 0;
+    var counter_h4 = 0;
+    var counter_h5 = 0;
 
-    $('h1, h2, h3').each(function() {
-        if ( $(this).prop('tagName') == 'H1' )
+    $('h3, h4, h5').each(function() {
+        if ( $(this).prop('tagName') == 'H3' )
         {
-            counter_h1 += 1;
-            counter_h2  = 0;
-            counter_h3  = 0;
+            counter_h3 += 1;
+            counter_h4  = 0;
+            counter_h5  = 0;
 
-	    this.label = "ARTICLE " + counter_h1;
+	    this.label = "ARTICLE " + counter_h3;
 
             $(this).before("<div class='index bold'>" + this.label + "</div>");
         }
 
-        if ( $(this).prop('tagName') == 'H2' )
+        if ( $(this).prop('tagName') == 'H4' )
         {
-            counter_h2 += 1;
-            counter_h3  = 0;
+            counter_h4 += 1;
+            counter_h5  = 0;
 
-	    this.label = "Section " + counter_h1 + "." + counter_h2;
+	    this.label = "Section " + counter_h3 + "." + counter_h4;
 
             $(this).before("<div class='index'>" + this.label + "</div>");
         }
 
-        if ( $(this).prop('tagName') == 'H3' )
+        if ( $(this).prop('tagName') == 'H5' )
         {
-            counter_h3 += 1;
+            counter_h5 += 1;
 
-	    this.label = counter_h1 + "." + counter_h2 + "." + counter_h3;
+	    this.label = counter_h3 + "." + counter_h4 + "." + counter_h5;
 
             $(this).before("<div class='index padded'>" + this.label + "</div>");
         }
